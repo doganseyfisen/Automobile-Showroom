8d783e58940a25f514fc2f4d59a5aeba89bfc71a
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2023-04-15 02:44:13 +0300

    Update and rename workOnDatabase.py to work_on_database.py
    PEP8

--- work_on_database.py
+++ work_on_database.py
@@ -1,6 +1,7 @@
 import time
 
-from autoShowroom import *
+from auto_showroom import *
+
 print(""""
 === WELCOME TO AUTO-SHOWROOM ===
 
@@ -16,22 +17,27 @@ What do you want to do?
 
 =================================
 """)
+
 showroom = Showroom()
 while True:
     userSelect = input("Your request: ")
     print("\n")
-    if (userSelect == "q" or userSelect == "Q"):
+
+    if userSelect == "q" or userSelect == "Q":
         print("Goodbye!")
         break
-    elif (userSelect == "1"):
-        showroom.showAllAutos()
-    elif (userSelect == "2"):
+
+    elif userSelect == "1":
+        showroom.show_all_autos()
+
+    elif userSelect == "2":
         brand = input("Which brand do you want to query: ")
         print("Querying now...")
         time.sleep(2)
-        showroom.queryAutos(brand)
+        showroom.query_autos(brand)
         print("Queried.\n")
-    elif (userSelect == "3"):
+
+    elif userSelect == "3":
         brand = input("Brand: ")
         model = input("Model: ")
         year = int(input("Year: "))
@@ -42,35 +48,40 @@ while True:
         newAuto = Automobile(brand, model, year, price, hp, transmission, cc)
         print("Adding now...")
         time.sleep(2)
-        showroom.addAuto(newAuto)
+        showroom.add_auto(newAuto)
         print("Added.\n")
-    elif (userSelect == "4"):
+
+    elif userSelect == "4":
         brand = input("Brand: ")
         model = input("Model: ")
         year = int(input("Year: "))
         answerIs = input("Do you want to continue deleting process? Y/N: ")
-        if (answerIs == "Y" or answerIs == "y"):
+
+        if answerIs == "Y" or answerIs == "y":
             print("Deleting now...")
             time.sleep(2)
-            showroom.deleteAuto(brand, model, year)
+            showroom.delete_auto(brand, model, year)
             print("Deleted.\n")
-    elif (userSelect == "5"):
+
+    elif userSelect == "5":
         brand = input("Brand: ")
         model = input("Model: ")
         year = input("Year: ")
         cc = int(input("CC: "))
-        showroom.raisePrice(brand, model, year, cc)
+        showroom.raise_price(brand, model, year, cc)
         print("Raising now....")
         time.sleep(2)
         print("Raised.\n")
-    elif (userSelect == "6"):
+
+    elif userSelect == "6":
         brand = input("Brand: ")
         model = input("Model: ")
         year = input("Year: ")
         cc = int(input("CC: "))
-        showroom.reducePrice(brand, model, year, cc)
+        showroom.reduce_price(brand, model, year, cc)
         print("Reducing now....")
         time.sleep(2)
         print("Reduced.\n")
+
     else:
         print("Invalid request!\n")
